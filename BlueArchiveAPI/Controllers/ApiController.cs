46d698d4c0e6e17099b5ac8cf2ac7b5c8aa86ca7
cc004 <1176321897@qq.com>
2023-04-02 03:50:48 +0800

    serialize test passed

--- BlueArchiveAPI/Controllers/ApiController.cs
+++ BlueArchiveAPI/Controllers/ApiController.cs
@@ -61,6 +61,22 @@ namespace BlueArchiveAPI.Controllers
             var respData = Utils.DecryptResponsePacket(content, out proto);
             
             _logger.LogInformation($"{respData}");
+
+            if (proto == Protocol.Account_LoginSync)
+            {
+                var respObj = respData.ToObject<AccountLoginSyncResponse>();
+                foreach (var character in respObj.CharacterListResponse.CharacterDBs)
+                {
+                    character.ExSkillLevel = 100;
+                    character.ExtraPassiveSkillLevel = 100;
+                    character.LeaderSkillLevel = 100;
+                    character.PublicSkillLevel = 100;
+                    character.ExtraPassiveSkillLevel = 100;
+                    character.Level = 100;
+                }
+
+                respData = JToken.FromObject(respObj);
+            }
             
             return File(Utils.EncryptResponsePacket(respData, proto), "application/json; charset=utf-8");
         }
